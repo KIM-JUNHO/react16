456d150ae83532f04f17865daf27abd6946b59bc
prpr48 <prpr48@naver.com>
2020-02-29 22:58:33 +0900

    old style one return by div

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+
+class ReturnTypes extends Component {
+  render() {
+    return (
+      <span>
+        <header></header>
+        <div></div>
+        <footer></footer>
+      </span>
+    );
+  }
+}
 
 function App() {
   return <div className="App"></div>;
