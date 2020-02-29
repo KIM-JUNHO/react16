0deefa99049e6210851f469fd255170ccdb59985
prpr48 <prpr48@naver.com>
2020-02-29 23:17:43 +0900

    make error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component, Fragment } from "react";
 import { createPortal } from "react-dom";
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
 class Portals extends Component {
+  componentDidCatch = (error, info) => {
+    console.log(`cached ${error} the info i have is ${JSON.stringify(info)}`);
+  };
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
   }
@@ -20,6 +40,7 @@ function App() {
     <Fragment>
       <ReturnTypes />
       <Portals />
+      <ErrorMaker />
     </Fragment>
   );
 }
